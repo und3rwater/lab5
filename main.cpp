4682140e6d669a85f3430ee7823ca0116a4bcf04
Und3r <slopal0550@gmail.com>
2025-12-10 14:22:10 +0500

    Каркас для трех функций

--- main.cpp
+++ main.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
+vector<string> ReadFile(const string& filename) // считывание из файла набор строк в вектор функция № 1
+{
+
+    return lines;
+}
+
+
+void DisplayFile(const vector<string>& lines) // вывод строк на экран (функция № 2)
+{
+
+}
+
+void FileOut(const vector<string>& lines, const string& filename) // запись в файл (функция № 3)
+{
+
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    setlocale(LC_ALL, "Russian");
+
+    vector<string> lines = ReadFile("input.txt");
+    DisplayFile(lines);
+    FileOut(lines, "output.txt");
+
     return 0;
 }
